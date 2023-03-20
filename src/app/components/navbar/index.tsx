dd9496e5c55d091e616c4e29343b1ef7bcbb986a
egi <radek@dulisz.com>
2023-03-20 15:51:23 +0100

    navbar-add-hamburger-menu

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 import { Logo } from "../logo";
+import { NavItems } from "./navitems";
+import { Slide as Menu } from "react-burger-menu";
 
 const NavbarContainer = styled.div`
   min-hheight: 68px;
   ${tw`
     w-full
-    max-w-2xl
+    max-w-screen-2xl
     flex
     flex-row
     items-center
@@ -25,6 +27,7 @@ export function NavBar() {
       <LogoContainer>
         <Logo></Logo>
       </LogoContainer>
+      <NavItems></NavItems>
     </NavbarContainer>
   );
 }
